a9170b8bbc72def6853410fe183c1b866a70d32d
probe <probe>
2015-05-23 18:21:48 -0700

    hash mac address before sending out

--- server/app/query.py
+++ server/app/query.py
@@ -1,4 +1,4 @@
-import time
+import time, hashlib
 
 
 from cassandra.util import max_uuid_from_time
@@ -14,58 +14,7 @@ HOUR_24 = 60 * 24
 
 class Select(object):
    
-    def aggregate_recent(self, resultset):
-        # aggregate each device into a seperate group
-        # sum total of record signal then divide by number
-        # of records to get average signal
-        devices = {}
-        for record in resultset:
-            if record.mac not in devices:
-                devices[record.mac] = {
-                    "count":0,
-                    "signal_total":0,
-                    "signal_count":0,
-                    "last_arrival":record.arrival
-                }
-            devices[record.mac]["count"] += 1
-            devices[record.mac]["first_arrival"] = record.arrival
-            if record.signal:
-                devices[record.mac]["signal_total"] += record.signal
-                devices[record.mac]["signal_count"] += 1
-        # formate data export
-        # each row is a list of records with the
-        # index denoted by the fields list
-        summary = []
-        for mac, data in devices.iteritems():
-            summary.append([
-                mac,
-                float(data["first_arrival"]),
-                float(data["last_arrival"]),
-                data["signal_total"] / data["signal_count"],
-                data["count"]
-            ])
-        first_arrival = resultset[len(resultset) - 1]
-        recent_arrival = resultset[0].arrival
-        return {
-            "data":summary,
-            "fields":["mac", "first_arrival", "last_arrival", "avg_signal", "count"]
-        }
-
-
-    @query_wrapper
-    def recent(self, location="", age=15):
-        # query cassandra table using location
-        # as partition key and using age in minutes
-        # as filter
-        recent = time.time() - age * 60
-        resultset = Recent.objects\
-            .filter(location=location)\
-            .filter(Recent.stamp > max_uuid_from_time(recent))
-        return self.aggregate_recent(resultset)
-
-
-    @query_wrapper
-    def visits(self, location="", age=24*60, min_records=0):
+    def visits(self, location="", age=HOUR_24, min_records=0):
         recent = time.time() - age * 60
         resultset = LocationIndex.objects\
             .filter(location=location)\
@@ -79,9 +28,13 @@ class Select(object):
                     visit.signals,
                     visit.counts
                 )
+                mac_hash = hashlib.md5(record.mac).hexdigest()[0:6]
+                if visit.manuf:
+                    device = visit.manuf + "-" + mac_hash
+                else: device = mac_hash
+                
                 visits.append({
-                    "mac":record.mac,
-                    "manuf":visit.manuf,
+                    "device":device,
                     "first_arrival":float(visit.first_arrival),
                     "recent_arrival":float(visit.recent_arrival),
                     "data":data
