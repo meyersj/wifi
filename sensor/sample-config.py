97bebd4cfeed020bdfdbf6f1794827a6eda1b4b6
probe <probe>
2015-05-23 17:25:08 -0700

    remove old stuff

--- sensor/sample-config.py
+++
@@ -1,7 +0,0 @@
-interface = "<interface>"
-sensor = "<sensor name>"
-location = "<location of sensor>"
-sensor_mac = "<mac address of network card>"
-endpoint = "<endpoint for packets>"
-interval=5
-timeout=55
