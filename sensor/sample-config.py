b7472c9accfa3a7d634cbeb55c90c7b134f51cb5
Jeffrey Meyers <meyers2@pdx.edu>
2015-05-27 11:01:21 -0700

    change configs

--- sensor/sample-config.py
+++ sensor/sample-config.py
@@ -4,5 +4,5 @@ location = "<location of sensor>"
 sensor_mac = "<mac address of network card>"
 endpoint = "<endpoint for packets>"
 interval= 30    # how often packets are sent over the network
-timeout= 55     # total time to listen for packets
+timeout= 295    # total time to listen for packets
 unique = 5      # min time interval for distinct (sub, sa, ta, da, ra)
