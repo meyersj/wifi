05fe028b641c737ebf541b0c3dfcaf0d60605612
Jeffrey Meyers <meyers2@pdx.edu>
2015-07-28 16:37:47 -0700

    clean up endpoints

--- server/app/endpoints.py
+++ server/app/endpoints.py
@@ -1,9 +1,8 @@
 from flask import request, jsonify
 
 from app import app, debug, error
-#from processor import Processor, Stream
+from request_processor import Processor
 
-#from query import Select
 
 
 @app.route('/', methods=['GET'])
@@ -13,39 +12,7 @@ def index():
 @app.route('/data', methods=['POST'])
 def data_stream():
     processor = Processor(data=request.data)
-    return jsonify(response=True)
-    #return processor.run()
+    response = processor.run()
+    return response
 
 
-"""
-@app.route('/stream', methods=['POST'])
-def stream():
-    stream = Stream(data=request.data)
-    return stream.run()
-
-@app.route('/visits', methods=['GET'])
-def visits():
-    select = Select()
-    location = request.args.get("location")
-    age = request.args.get("age")
-    rate = request.args.get("rate")
-    args = {}
-    if location: args["location"] = location
-    if age: args["age"] = int(age)
-    if rate: args["rate"] = int(rate)
-    data = select.visits(**args)
-    data["success"] = True
-    return jsonify(data)
-
-@app.route('/visitor-history', methods=['GET'])
-def visitor_history():
-    select = Select()
-    mac = request.args.get("mac")
-    args = {}
-    data = {"success":False}
-    if mac:
-        args["mac"] = mac
-        data = select.visitor_history(**args)
-        data["success"] = True
-    return jsonify(data)
-"""
