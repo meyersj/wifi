7ea088ed5b9c35a826e055faac42ee0231e1ccea
Jeffrey Meyers <meyers2@pdx.edu>
2015-07-28 17:24:21 -0700

    use sqlalchemy for db orm

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
+
 app.debug = True
+
 debug = app.logger.debug
 error = app.logger.error
 
