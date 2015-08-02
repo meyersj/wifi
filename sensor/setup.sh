d0e5bd7309d84af028de98f4221976209420255e
Jeffrey Meyers <meyers2@pdx.edu>
2015-08-02 13:46:28 -0700

    use pip for pyshark install

--- sensor/setup.sh
+++ sensor/setup.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 virtual=$PWD/env/bin
-archive=KimiNewt-pyshark-v0.3.4-20-geb4f2b9.tar.gz
-install=KimiNewt-pyshark-eb4f2b9/src
 
 rm -fr env
 virtualenv env
 cp -n sample-config.py config.py
 $virtual/pip install -r requirements.txt
-cd lib
-tar xzvf ${archive}
-cd ${install}
-$virtual/python setup.py install
