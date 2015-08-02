c962197168d2342159e7b3ef5f8950ceb3edf10b
Jeffrey Meyers <meyers2@pdx.edu>
2015-08-01 19:41:50 -0700

    minor change to app views

--- server/app/mod_app/views.py
+++ server/app/mod_app/views.py
@@ -1,12 +1,17 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
 
 from app import app, debug, error
 
 
-mod_app = Blueprint('app', __name__, url_prefix='/')
+mod_app = Blueprint('app', __name__, url_prefix='/app')
 
-@mod_app.route('/')
+@mod_app.route('/test')
 def index():
-    return "root"
+    return "app root"
+    
+
+@mod_app.route('/devices')
+def devices():
+    return render_template("devices.html")
 
 
