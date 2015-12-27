f76df797a0f01a14e0c3a93de2c77a8b6a0c4754
Jeff Meyers <jeffrey.alan.meyers@gmail.com>
2015-12-27 12:45:58 -0800

    modify setup.sh to use conf directory

--- sensor/setup.sh
+++ sensor/setup.sh
@@ -5,5 +5,5 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 rm -fr $SCRIPTPATH/env
 virtualenv $SCRIPTPATH/env
-cp -n $SCRIPTPATH/sample-config.py $SCRIPTPATH/config.py
-$SCRIPTPATH/env/bin/pip install -r $SCRIPTPATH/requirements.txt
+cp -n $SCRIPTPATH/conf/sample-config.py $SCRIPTPATH/conf/config.py
+$SCRIPTPATH/env/bin/pip install -r $SCRIPTPATH/conf/requirements.txt
