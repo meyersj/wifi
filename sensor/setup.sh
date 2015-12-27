bbe166add6419b8bacbf3d1ffc39c5d2e2e7bc99
Jeff Meyers <jeffrey.alan.meyers@gmail.com>
2015-12-27 12:04:36 -0800

    refactor setup.sh to make sure path to sensor directory is correct

--- sensor/setup.sh
+++ sensor/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-virtual=$PWD/env/bin
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
 
-rm -fr env
-virtualenv env
-cp -n sample-config.py config.py
-$virtual/pip install -r requirements.txt
+rm -fr $SCRIPTPATH/env
+virtualenv $SCRIPTPATH/env
+cp -n $SCRIPTPATH/sample-config.py $SCRIPTPATH/config.py
+$SCRIPTPATH/env/bin/pip install -r $SCRIPTPATH/requirements.txt
