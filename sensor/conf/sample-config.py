d8bbbf081156f99e7a9439aab1ba32c25380c4d4
Jeffrey Meyers <meyers2@pdx.edu>
2015-12-28 10:39:02 -0800

    update sample-config to have endpoint

--- sensor/conf/sample-config.py
+++ sensor/conf/sample-config.py
@@ -1,4 +1,5 @@
-interface = "mon0"                              # interface for airmon-ng   required
-sensor_mac = "XX:XX:XX:XX:XX:XX"                # mac address of wifi card  required
-sensor = "default"                              # name of sensor            [optional]
-location = "default"                            # name of location          [optional]
+interface = "mon0"                          # interface for airmon-ng       required
+sensor_mac = "XX:XX:XX:XX:XX:XX"            # mac address of wifi card      required
+sensor = "default"                          # name of sensor                [optional]
+location = "default"                        # name of location              [optional]
+endpoint = "http://127.0.0.1:5000/packet"   # endpoint to receive packets   [optional]
