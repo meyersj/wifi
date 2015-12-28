ff68ada4989331d43c8b20f542a6873d7d2a17c6
Jeffrey Meyers <meyers2@pdx.edu>
2015-12-28 10:41:26 -0800

    add endpoint handler for go server

--- go-server/main.go
+++ go-server/main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strconv"
+	"./handlers"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
-func packet_handler(w http.ResponseWriter, r *http.Request) {
-    // set response headers
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Content-type", "application/json")
-    
-    // send response
-    fmt.Fprintf(w, "Hello")
+func attach_handlers() {
+	http.HandleFunc("/", handlers.Index)
+	http.HandleFunc("/packet", handlers.Packet)
 }
 
 func start_server(port int) {
-    port_str := ":" + strconv.Itoa(port)
-    fmt.Println("Starting server at 127.0.0.1" + port_str)
-    http.HandleFunc("/packet", packet_handler)
-    http.ListenAndServe(port_str, nil)
+	port_str := ":" + strconv.Itoa(port)
+	fmt.Println("Starting server at 127.0.0.1" + port_str)
+	attach_handlers()
+	log.Fatal(http.ListenAndServe(port_str, nil))
 }
 
 func main() {
-    start_server(5000)
+	start_server(5000)
 }
