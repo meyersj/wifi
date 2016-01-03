7cd73b070339515c6b3c2cfd3a32327cf89a4841
Jeff Meyers <jeffrey.alan.meyers@gmail.com>
2016-01-02 16:24:06 -0800

    change default port in sensor config

--- sensor/conf/sample-config.py
+++ sensor/conf/sample-config.py
@@ -2,4 +2,4 @@ interface = "mon0"                          # interface for airmon-ng       requ
 sensor_mac = "XX:XX:XX:XX:XX:XX"            # mac address of wifi card      required
 sensor = "default"                          # name of sensor                [optional]
 location = "default"                        # name of location              [optional]
-endpoint = "http://127.0.0.1:5000/packet"   # endpoint to receive packets   [optional]
+endpoint = "http://127.0.0.1:8005/packet"   # endpoint to receive packets   [optional]
