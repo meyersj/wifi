f3154f9fc61d46a036736d54535aa9110c59bb6c
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-05 17:30:17 -0800

    update sample config

--- sensor/conf/sample-config.py
+++ sensor/conf/sample-config.py
@@ -2,4 +2,6 @@ interface = "mon0"                          # interface for airmon-ng       requ
 sensor_mac = "XX:XX:XX:XX:XX:XX"            # mac address of wifi card      required
 sensor = "default"                          # name of sensor                [optional]
 location = "default"                        # name of location              [optional]
-endpoint = "http://127.0.0.1:8005/packet"   # endpoint to receive packets   [optional]
+endpoint = "http://127.0.0.1:8005/packet"   # endpoint to receive packets   required
+payload_timer = 5                           # seconds between requests
+distinct_timer = 15                         # time between distinct (src, dst) packets 
