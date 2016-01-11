b40ccf55394b65b7e99b7a49ba3e3274a729e40c
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-10 19:13:06 -0800

    update tests

--- go-server/handlers_test.go
+++ go-server/handlers_test.go
@@ -4,6 +4,7 @@ import (
 	"./utils"
 	"./wifiproto"
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"github.com/golang/protobuf/proto"
 	"io/ioutil"
@@ -14,23 +15,57 @@ import (
 )
 
 var conf *Config = ReadConfig("config.toml")
+var arrival float64 = 1451328466.85
 
-func BuildPostPayload() *bytes.Buffer {
-	packet := &wifiproto.Packet{
-		Arrival:     proto.Float64(1451328466.85),
+func BuildTestPacket(offset float64) *wifiproto.Packet {
+	return &wifiproto.Packet{
+		Arrival:     proto.Float64(arrival + offset),
 		Subtype:     proto.String("0x04"),
 		Source:      proto.String("00:00:00:00:00:00"),
 		Destination: proto.String("FF:FF:FF:FF:FF:FF"),
 		Freq:        proto.Int32(2437),
 		Signal:      proto.Int32(-50),
 	}
+}
 
-	data, err := proto.Marshal(packet)
+func BuildTestPayload() *bytes.Buffer {
+	packet1 := BuildTestPacket(0)
+	packet2 := BuildTestPacket(15)
+	packet3 := BuildTestPacket(350)
+
+	location := "Test Location"
+	sensor := "Test Sensor"
+
+	payload := &wifiproto.Payload{
+		Location: &location,
+		Sensor:   &sensor,
+		Data:     []*wifiproto.Packet{packet1, packet2, packet3},
+	}
+
+	data, err := proto.Marshal(payload)
 	if err != nil {
 		log.Fatal("marshaling error: ", err)
 	}
-
 	return bytes.NewBuffer(data)
+}
+
+func CleanupTestDB(db *sql.DB) {
+	delete_packets := "DELETE FROM data.packets WHERE src = '00:00:00:00:00:00';"
+	delete_bucket5 := "DELETE FROM data.bucket5 WHERE mac = '00:00:00:00:00:00';"
+	delete_hourly := "DELETE FROM data.hourly WHERE mac = '00:00:00:00:00:00';"
+	_, err := db.Exec(delete_packets)
+	if err != nil {
+		log.Fatal("failed to delete test data <data.packets>: ", err)
+	}
+	_, err = db.Exec(delete_bucket5)
+	if err != nil {
+		log.Fatal("failed to delete test data <data.bucket5>: ", err)
+	}
+	_, err = db.Exec(delete_hourly)
+	if err != nil {
+		log.Fatal("failed to delete test data <data.hourly>: ", err)
+	}
+
 }
 
 func TestIndexHandler(t *testing.T) {
@@ -44,10 +79,11 @@ func TestIndexHandler(t *testing.T) {
 func TestPostPacketHandler(t *testing.T) {
 	db := InitDBClient(conf.Postgres)
 	handler := PacketHandler{Db: db}
-	defer handler.Db.DB.Close()
+	defer db.DB.Close()
+	defer CleanupTestDB(db.DB)
 
 	// construct test data
-	payload := BuildPostPayload()
+	payload := BuildTestPayload()
 
 	req, _ := http.NewRequest("POST", "/packet", payload)
 	rec := httptest.NewRecorder()
