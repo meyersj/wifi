cf75f45bde01c85340259e859e84a742c295dfcb
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-10 22:21:27 -0800

    download summary of hour data on hover

---
+++ cache.js
@@ -0,0 +1,22 @@
+function DataCache () {
+    this.data = {};
+}
+
+DataCache.prototype = {
+    constructor: DataCache,
+    get: function(key) {
+        if (this.data.hasOwnProperty(key)) {
+            var now = new Date().valueOf() / 1000;
+            if (this.data[key].expiration > now) {
+                return this.data[key].data;
+            }
+        }
+        return null;
+    },
+    add: function(key, data, ttl) {
+        this.data[key] = {
+            expiration: (new Date().valueOf() / 1000) + ttl,
+            data: data
+        };
+    }
+}
