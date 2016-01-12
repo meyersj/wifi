fba1b27d743e391b00594ab6632d2eea966115a7
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-11 16:16:01 -0800

    clean up a little bit

---
+++ scales.js
@@ -0,0 +1,47 @@
+function buildAgeScale(start, window_size) {
+    return d3.scale.pow().exponent(1/2)
+        .domain([start + (window_size * 60), start])
+        .range(["blue", "white"]);
+}
+
+function buildDurationScale(window_size) {
+    return d3.scale.linear()
+        .domain([0, window_size * 60])
+        .range([5, 15]);
+}
+
+function buildXScale(w) {
+    return d3.scale.linear()
+        .domain([-30, -100])
+        .range([0 + 20, w - 20]);
+}
+
+function buildYScale(h, start, window_size) {
+    return d3.scale.linear()
+        .domain([start, start + (60 * window_size)])
+        .range([h - 20, 0 + 20]);
+}
+
+function buildHourRadiusScale() {
+    // ping count
+    var MAX_PING_PER_HOUR = 12;
+    var MIN_RADIUS = 5;
+    var MAX_RADIUS = 15;
+    return d3.scale.linear()
+        .domain([1, MAX_PING_PER_HOUR])
+        .range([MIN_RADIUS, MAX_RADIUS]);
+}
+
+function buildHourColorScale() {
+    // signal strength
+    return d3.scale.linear()
+        .domain([-30, -100])
+        .range(["#3E932A", "white"]);
+}
+
+function buildHourXScale(w, start_time) {
+    // x coordinate
+    return d3.scale.linear()
+        .domain([start_time, start_time + 3600])
+        .range([0 + 20, w - 20]);
+}
