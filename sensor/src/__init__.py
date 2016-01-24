be93be44a83afa02a72e088dfda02d1f073a478c
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-24 12:15:48 -0800

    update logic to find newrelic config file for sensor

--- sensor/src/__init__.py
+++ sensor/src/__init__.py
@@ -1,10 +1,25 @@
+import os
+
+
+ENV = "NEW_RELIC_CONFIG_FILE"
+
 """
-try to create newrelic application if the agent is installed
+these two objects will be imported from PostHandler,
+if they are empty new relic monitoring will not happen
 """
 application = None
 agent = None
-try:
+
+monitor = False
+config = os.environ.get(ENV, "")
+if config and os.path.isfile(config):
+    monitor = True
+else:
+    config = os.path.abspath("../newrelic/newrelic.ini")
+    print config
+    if os.path.isfile(config):
+        monitor = True
+        os.environ[ENV] = config
+if monitor:
     import newrelic.agent as agent
     application = agent.application()
-except:
-    pass
