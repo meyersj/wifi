96162fc3ecb3e4c989cef32bc87c4dd228d2ec30
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-24 12:23:41 -0800

    add newrelic monitoring using gorelic plugin for go-server api

--- go-server/main.go
+++ go-server/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/BurntSushi/toml"
+	"github.com/yvasiyarov/gorelic"
 	"log"
 )
 
 type Config struct {
 	Port     string
 	Postgres string
+	NewRelic string
 }
 
 func ReadConfig(filename string) *Config {
@@ -21,5 +23,11 @@ func ReadConfig(filename string) *Config {
 
 func main() {
 	conf := ReadConfig("config.toml")
+	if conf.NewRelic != "" {
+		agent := gorelic.NewAgent()
+		agent.Verbose = true
+		agent.NewrelicLicense = conf.NewRelic
+		agent.Run()
+	}
 	StartServer(conf)
 }
