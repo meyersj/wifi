4417fe032433c5a4e8142a22765b1816bc624b6a
Jeffrey Meyers <meyers2@pdx.edu>
2016-01-24 18:16:25 -0800

    fix path to default config

--- sensor/src/__init__.py
+++ sensor/src/__init__.py
@@ -10,16 +10,18 @@ if they are empty new relic monitoring will not happen
 application = None
 agent = None
 
+filepath = os.path.dirname(os.path.realpath(__file__))
+default_config = os.path.join(os.path.dirname(filepath), "newrelic/newrelic.ini")
 monitor = False
 config = os.environ.get(ENV, "")
 if config and os.path.isfile(config):
     monitor = True
 else:
-    config = os.path.abspath("../newrelic/newrelic.ini")
-    print config
+    config = default_config
     if os.path.isfile(config):
         monitor = True
         os.environ[ENV] = config
 if monitor:
+    print "MONITORING"
     import newrelic.agent as agent
     application = agent.application()
