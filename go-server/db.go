755349a76e50db42610a4193b8144c55660725e2
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-23 17:09:30 -0700

    add manufacture lookup

--- go-server/db.go
+++ go-server/db.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 	"log"
+	"strings"
 )
 
 type DBClient struct {
@@ -26,13 +27,27 @@ func InitDBClient(uri string) *DBClient {
 }
 
 func (c *DBClient) InsertPacket(p *wifiproto.Packet) {
+
+	var src_manuf, dest_manuf string
+	src_prefix := strings.ToUpper(string(*p.Source)[0:8])
+	dest_prefix := strings.ToUpper(string(*p.Destination)[0:8])
+	c.DB.QueryRow(
+		"SELECT manuf FROM data.manuf WHERE prefix = $1", src_prefix,
+	).Scan(&src_manuf)
+	c.DB.QueryRow(
+		"SELECT manuf FROM data.manuf WHERE prefix = $1", dest_prefix,
+	).Scan(&dest_manuf)
+
 	stmt := "INSERT INTO data.packets " +
-		"(arrival, subtype, src, dest, freq, signal) " +
+		"(arrival, subtype, src, src_manuf, dest, dest_manuf, freq, signal) " +
 		"VALUES " +
-		"($1, $2, $3, $4, $5, $6)"
+		"($1, $2, $3, $4, $5, $6, $7, $8)"
 	_, err := c.DB.Exec(
 		stmt,
-		p.Arrival, p.Subtype, p.Source, p.Destination, p.Freq, p.Signal,
+		p.Arrival, p.Subtype,
+		p.Source, src_manuf,
+		p.Destination, dest_manuf,
+		p.Freq, p.Signal,
 	)
 	if err != nil {
 		log.Fatal("failed to insert packet:", err)
