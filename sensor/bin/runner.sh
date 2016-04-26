19df9779857f003c2d51ef328a256ab47969d8aa
Jeffrey Meyers <meyers2@pdx.edu>
2016-04-25 17:27:46 -0700

    supress output when running with test runner

--- sensor/bin/runner.sh
+++ sensor/bin/runner.sh
@@ -5,7 +5,5 @@ SCRIPTPATH=`dirname $SCRIPT`
 PARENT=`dirname $SCRIPTPATH`
 
 cd $PARENT
-
 export PATH=$PATH:$SCRIPTPATH
-
-./env/bin/python src/runner.py
+./env/bin/python src/runner.py > /dev/null
