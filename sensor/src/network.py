2ece1fa9e64419d30dddd20dbdf4ccbc3f0afec5
Jeff <jeff@raspberrypi>
2016-05-08 13:53:49 -0700

    cleanup network after pylint

--- sensor/src/network.py
+++ sensor/src/network.py
@@ -1,19 +1,28 @@
-import subprocess
+""" utility functions for working with network interfaces """
+
+
 import os
-import sys
-import logging
+import socket
+import fcntl
+import struct
 
 
-def is_monitoring(interface):
+def get_mac_address(ifname):
+    """
+    get hardware address from network interface name
+
+    source: https://gist.github.com/zhenyi2697/6080400
+    """
+    if not is_available(ifname):
+        return False
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(sock.fileno(), 0x8927, struct.pack('256s', ifname[:15]))
+    return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+
+
+def is_available(interface):
+    """ check if interface provided by name exists """
     network_interfaces = os.listdir('/sys/class/net/')
     if interface not in network_interfaces:
         return False
     return True
-
-
-def start_monitoring(interface):
-    try:
-        return subprocess.call(['startmon'])
-    except OSError:
-        # this envvar will be set in the init.d script if running as a service
-        return subprocess.call([os.path.join(os.getenv('WIFIBIN', ''), 'startmon')])
