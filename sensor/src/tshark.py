1c2d6e3dac5f79be5670917e3753f89640ee75ef
Jeff <jeff@raspberrypi>
2016-05-08 13:07:17 -0700

    cleanup tshark after linting

--- sensor/src/tshark.py
+++ sensor/src/tshark.py
@@ -1,3 +1,7 @@
+"""
+Construct tshark command to be ran as subprocess
+"""
+
 import os
 
 
@@ -15,40 +19,39 @@ SUBTYPES = ['0x04', '0x05', '0x20', '0x28']
 
 
 class TSharkBuilder(object):
-   
-    def __init__(self, interface):
+    """ helper class to construct tshark command """
+
+    def __init__(self, interface='wlan0'):
         self.interface = interface
         self.fields = FIELDS
-    
+        self.subtypes = SUBTYPES
+
     def build(self):
-        bpf_filter = "not broadcast and not multicast"
-        tshark = "tshark -i {0} -l -f '{1}' -Y '{2}' -T fields {3}"
-        display_filter = "({0}) && ({1})".format(self._subtypes(), self._macs())
-        return tshark.format(
-                self.interface, bpf_filter, display_filter, self._fields()
-        ) 
+        """ construct tshark command """
+        #bpf_filter = "not broadcast and not multicast"
+        #tshark = "tshark -i {0} -l -f '{1}' -Y '{2}' -T fields {3}"
+        tshark = "tshark -i {0} -l -Y '{1}' -T fields {2}"
+        return tshark.format(self.interface, self._subtypes(), self._fields())
 
     def set_fields(self, fields):
+        """ set fields that tshark should output """
         self.fields = fields
+        return self
 
     def set_subtypes(self, subtypes):
+        """ set subtype of packets that should be included in output """
         self.subtypes = subtypes
-    
-    def set_macs(self, macs):
-        self.macs = macs
-    
+        return self
+
     def _fields(self):
+        """ construct fields that tshark should output """
         cmd = "-e {0}"
         return " ".join([cmd.format(field) for field in self.fields])
-    
-    def _subtypes(self):
-        filter_ = "wlan.fc.type_subtype == {0}"
-        if self.subtypes:
-            return " || ".join([filter_.format(subtype) for subtype in self.subtypes])
-        return ""
 
-    def _macs(self):
-        filter_ = "wlan.sa != {0}"
-        if self.macs:
-            return " && ".join([filter_.format(mac) for mac in self.macs])
+    def _subtypes(self):
+        """ construct subtype display filter """
+        filter_ = "wlan.fc.type_subtype == {0}"
+        filters = [filter_.format(subtype) for subtype in self.subtypes]
+        if self.subtypes:
+            return " || ".join(filters)
         return ""
