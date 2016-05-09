1e268ad7c9bffbd36aef2a6d05844c51e976df38
Jeff <jeff@raspberrypi>
2016-05-08 17:16:24 -0700

    cleanup scripts

--- sensor/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$(readlink -f $0)
-SCRIPTPATH=`dirname $SCRIPT`
-
-rm -fr $SCRIPTPATH/env
-virtualenv $SCRIPTPATH/env
-cp -n $SCRIPTPATH/conf/sample-config.py $SCRIPTPATH/conf/config.py
-$SCRIPTPATH/env/bin/pip install -r $SCRIPTPATH/conf/requirements.txt
