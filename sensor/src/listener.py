75324e2d26fce9b186143f4dd1c97b57893a69c8
Jeff <jeff@raspberrypi>
2016-05-08 17:14:22 -0700

    refactor listener after running pylint

--- sensor/src/listener.py
+++ sensor/src/listener.py
@@ -1,28 +1,25 @@
+""" Start tshark subprocess and pass received packets to a handler """
+
+
 import os
-import sys
 import time
 import logging
 import random
-from uuid import uuid1
-from datetime import datetime
 import subprocess
 import signal
 
-#import pyshark
-
-from monitor import is_monitoring, start_monitoring
-from wifi_pb2 import Packet
+from .wifi_pb2 import Packet
 
 
 DEVNULL = open(os.devnull, 'w')
-
-
 # grab logger that has already been configured
-logger = logging.getLogger('wifi')
+LOGGER = logging.getLogger('wifi')
 
 
 def cast(value, new_type):
-    if value: return new_type(value)
+    """ cast value if exists """
+    if value:
+        return new_type(value)
     return value
 
 
@@ -33,41 +30,58 @@ class PacketProcessor(object):
     The process method will take as input a pyshark packet object
     and return a protobuf Packet object.
     """
-   
+
     def process(self, packet):
+        """ process output from tshark into protobuf 'Packet' object """
         out = Packet()
-        out.subtype = packet[1]
+        out.subtype = "0x" + hex(int(packet[1]))[2:].zfill(2)
         self.fetch_address_data(out, packet)
-        self.fetch_meta_data(out, packet) 
-        logger.debug('received packet: {0}'.format(out))
+        self.fetch_meta_data(out, packet)
         return out
-    
-    def fetch_address_data(self, p, packet):
+
+    def fetch_address_data(self, proto, packet):
+        """ grab source and destination fields from tshark output """
         source, destination = self.parse_addr(packet)
-        if source: p.source = source
-        if destination: p.destination = destination
-    
-    def fetch_meta_data(self, p, packet):
-        arrival, freq, signal = self.parse_meta(packet)
-        p.arrival = arrival
-        if freq: p.freq = freq
-        if signal: p.signal = signal
-        if p.subtype == "0x05": p.ssid = packet[2]
-   
-    def parse_addr(self, packet):
+        if source:
+            proto.source = source
+        if destination:
+            proto.destination = destination
+
+    def fetch_meta_data(self, proto, packet):
+        """ grab meta data from tshark output """
+        arrival, freq, signal_ = self.parse_meta(packet)
+        proto.arrival = arrival
+        if freq:
+            proto.freq = freq
+        if signal_:
+            proto.signal = signal_
+        if proto.subtype == "0x05":
+            proto.ssid = packet[2]
+
+    def parse_addr(self, packet):   # pylint: disable-msg=no-self-use
+        """ extract address fields via index """
         source = packet[3]
         destination = packet[4]
         return source, destination
-   
-    def parse_meta(self, packet):
+
+    def parse_meta(self, packet):   # pylint: disable-msg=no-self-use
+        """ extract meta data fields via index """
         arrival = float(packet[0])
         freq = cast(packet[5], int)
-        signal = cast(packet[6], int)
-        return arrival, freq, signal
+        signal_ = None
+        if len(packet) >= 7:
+            try:
+                # depending on tshark version sometimes signal fields
+                # has the form '-64' while sometimes it has the form '-64,-64'
+                signal_ = cast(packet[6], int)
+            except ValueError:
+                signal_ = cast(packet[6].split(',')[0], int)
+        return arrival, freq, signal_
 
-class Listener(object):
+
+class Listener(object): # pylint: disable-msg=too-few-public-methods
     """
-    The Listener will start sniffing for packets using the pyshark library.
+    The Listener will start sniffing for packets using a tshark subprocess
 
     Each received packet will be processed by the PacketProcessor object which
     will extract relavent data fields and build a Packet protobuf object.
@@ -76,22 +90,25 @@ class Listener(object):
     that object. The handler object must implement a handle method which
     takes as input the protobuf Packet object.
     """
-   
-    def __init__(self, config=None, cmd="", handler=None):
-        self.config = config
+
+    def __init__(self, cmd="tshark -Ii wlan0", handler=None):
         self.cmd = cmd
         self.processor = PacketProcessor()
-        if handler: self.handler = handler(config)
-        else: self.handler = None
+        if handler:
+            self.handler = handler()
+        else:
+            self.handler = None
 
     def start(self):
-        logger.info('starting listener')
+        """ start packet listener, loop forever in case of crash """
+        LOGGER.info('starting listener')
         while True:
             # if listener crashes because network card disconnects restart
             self._listen()
-            logger.error('listener stopped unexpectedly, restarting')
+            LOGGER.error('listener stopped unexpectedly, restarting')
 
     def _create_proc(self):
+        """ create tshark process and send output to pipe we will read from """
         return subprocess.Popen(
             self.cmd,
             shell=True,
@@ -101,60 +118,39 @@ class Listener(object):
         )
 
     def _listen(self):
-        # make sure monitoring interface is active
-        self._init_mon_interface()
-        # create tshark process
+        """ start process and read from output pipe """
         proc = self._create_proc()
         # continue sniffing forever (unless we crash)
         for line in iter(proc.stdout.readline, b''):
-            data =  line.rstrip().split('\t')
+            data = line.rstrip().split('\t')
             proto_packet = self.processor.process(data)
-            if self.handler: self.handler.handle(proto_packet)
-
-    def _init_mon_interface(self, delay=5):
-        interface = self.config.interface
-        if not is_monitoring(interface):
-            logger.info('start monitoring on interface {0}'.format(interface))
-            # try and start monitoring
-            start_monitoring(interface)
-            # if initial start failed, the network card might be in the process
-            # of reconnecting so wait a little before retrying
-            while not is_monitoring(interface) and delay < 360:
-                logger.warn('failed to start, waiting {0}s'.format(delay))
-                time.sleep(delay)
-                delay = delay * 2
-                msg = 'trying to start monitoring on interface {0}'
-                logger.warn(msg.format(interface))
-                start_monitoring(interface)
-        # check if we were succesful at starting to monitor interface
-        if not is_monitoring(interface):
-            logger.error("interface {0} is not currently available.".format(interface))
-            sys.exit(1)
+            if self.handler:
+                self.handler.handle(proto_packet)
 
 
-class SleepListener(Listener):
+class SleepListener(Listener):  # pylint: disable-msg=too-few-public-methods
     """ Listen for data packets (0x20, 0x28) for short intervals then sleep """
 
     def start(self):
-        logger.info('starting data listener')
+        """ start tshark listener, will go to sleep occasionally """
+        LOGGER.info('starting data listener')
         while True:
             # if listener crashes because network card disconnects restart
             self._listen()
-            logger.info('restarting after sleep')
+            LOGGER.info('restarting after sleep')
 
     def _listen(self):
         proc = self._create_proc()
         # continue sniffing until timeout
         start = time.time()
         for line in iter(proc.stdout.readline, b''):
-            packet =  line.rstrip().split('\t')
+            packet = line.rstrip().split('\t')
             if int(time.time()) - start > 10:
                 time.sleep(random.randrange(10, 20, 1))
-                # kill current process to sleep
+                # kill current process after sleep and exit loop
                 os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
-            now = float(time.time())
-            arrival = float(packet[0])
-            logger.debug("data {0}".format(now - arrival))
+                break
+            # process packet
             proto_packet = self.processor.process(packet)
             if self.handler:
                 self.handler.handle(proto_packet)
