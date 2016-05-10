373bb91c90b0b7243d54fae61622283d3c098c6e
Jeff <jeff@raspberrypi>
2016-05-10 16:38:53 -0700

    remove old runner script

--- sensor/bin/runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$(readlink -f $0)
-SCRIPTPATH=`dirname $SCRIPT`
-PARENT=`dirname $SCRIPTPATH`
-
-cd $PARENT
-export PATH=$PATH:$SCRIPTPATH
-./env/bin/python src/runner.py > /dev/null
